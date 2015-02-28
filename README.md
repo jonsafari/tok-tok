8f1388b25115e1085433ae54cb723f2f1c258f25
Jon Dehdari <jon@dehdari.org>
2015-02-28 21:33:44 +0100

    +tok-tok.pl; README.md++

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# tok-tok
+# Tok-tok
 A fast, simple, multilingual tokenizer
+
+
+## Overview
+
+Tok-tok is a fast, simple, multilingual tokenizer.
+It is freely licensed under the [Apache v2][] license.
+
+For example, given an input of:
+
+      They thought, "Is 9.5 or 525,600 my favorite number?",  before seeing Dr. Bob's dog talk on www.foo.ninja/index.html?nobeta=1&noalpha=0.
+
+The output is:
+
+      They thought , " Is 9.5 or 525,600 my favorite number ? " , before seeing Dr. Bob ' s dog talk on www.foo.ninja/index.html?nobeta=1&noalpha=0 .
+
+
+## Usage
+
+      perl tok-tok.pl [options] < text.txt > text.tok.txt
+
+      Options:
+       -h, --help        Print this usage
+       -d, --digit <u>   Conflate all digits to <u> . Note that 0 is reserved
+       -l, --lower       Lowercase text
+
+
+
+## Languages
+Tok-tok has been tested on, and gives reasonably good results for English, Persian, Russian, Czech, French, German, and a few others.
+The input should be in UTF-8 encoding.
+You can use the command-line tool `iconv` to convert other formats to UTF-8.
+
+
+## Name
+The name of the software means "*language*" or "*speech*" in Bislama, a Melanesian Creole language.
+[Bislama][] is quite possibly the world's perfect language.
+
+
+[Apache v2]: https://www.apache.org/licenses/LICENSE-2.0.html
+[Bislama]: https://en.wikipedia.org/wiki/Bislama
---
+++ tok-tok.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+## Simple, general tokenizer, where the input has one sentence per line (thus only final period is tokenized)
+## By Jon Dehdari, 2011-2015
+## Changes this:  They thought, "Is 9.5 or 525,600 my favorite number?"  before seeing Dr. Bob's dog talk.
+## To this:       They thought , " Is 9.5 or 525,600 my favorite number ? " before seeing Dr. Bob ' s dog talk .
+
+use v5.8.0;
+use strict;
+use Getopt::Long;
+use utf8;
+binmode(STDIN,  ":utf8");
+binmode(STDOUT, ":utf8");
+
+## Defaults
+my $lower = undef;
+my $digit = 0;
+
+my $usage     = <<"END_OF_USAGE";
+tok-tok.pl    (c) 2011-2015 Jon Dehdari - Apache License v2
+
+Usage:    perl $0 [options] < text.txt > text.tok.txt
+
+Function: A fast, simple, multilingual tokenizer
+
+Options:
+ -h, --help        Print this usage
+ -d, --digit <u>   Conflate all digits to <u> . Note that 0 is reserved
+ -l, --lower       Lowercase text
+
+END_OF_USAGE
+
+GetOptions(
+    'h|help|?'		=> sub { print $usage; exit; },
+    'd|digit=i'		=> \$digit,
+    'l|lower'		=> \$lower,
+) or die $usage;
+
+
+
+while (<>) {
+  m/^#/ && print && next;	# skip comments
+
+  s/ / /g;					# replace no-break spaces with normal spaces
+  s/([،;؛¿!"\])}»›”؟%٪°±©®।॥…])/ $1 /g;
+
+  ## URL-unfriendly characters: [:/?#]
+  s{:(?!//)}{ : }g;
+  s{\?(?!\S)}{ ? }g;
+  m{\S+\.\S+/\S+} or s{/}{ / }g; # not exactly right: doesn't tokenize legit slash if on same line as URL
+  s{ /}{ / }g;
+
+  s/& /&amp; /g;		# replace problematic character with numeric character reference
+  s/\t/ &#9; /g;		# replace problematic character with numeric character reference
+  s/\|/ &#124; /g;		# replace problematic character with numeric character reference
+  s/(\p{Open_Punctuation})/ $1 /g;
+  s/(\p{Close_Punctuation})/ $1 /g;
+  s/(,{2,})/ $1 /g;			# Fake German,Czech, etc.: „
+  s/(?<!,)([,،])(?![,\d])/ $1 /g;		# don't tokenize 1,000,000
+  s/([({\[“‘„‚«‹「『])/ $1 /g;
+  s/(['’`])/ $1 /g;		# just tokenize problematic hyphen/single quote, etc.
+  s/ ` ` / `` /g;		# stupid quotes
+  s/ ' ' / '' /g;		# stupid quotes
+  s/(\p{Currency_Symbol})/ $1 /g;
+  s/([–—])/ $1 /g;		# En dash and em dash
+  s/\s+$/\n/g;			# rm trailing spaces
+  s/(-{2,})/ $1 /g;			# Fake en-dash, etc.
+  s/(\.{2,})/ $1 /g;	# treat multiple periods as a thing (eg. ellipsis)
+  s/(?<!\.)\.$/ ./g;			# don't tokenize period unless it ends the line (and isn't preceded by another period)
+  s/(?<!\.)\.\s*(["'’»›”]) *$/ . $1/g;			# don't tokenize period unless it's near the end of the line: eg. " ... stuff."
+  s/^ +//g;				# rm leading  spaces
+  s/ {2,}/ /g;			# merge duplicate spaces
+
+  $digit and s/\d/$digit/g;
+
+  if ($lower) {
+    print lc;
+  } else {
+    print;
+  }
+}
