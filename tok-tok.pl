d07e9c808904e96217ce590d069b4043886077a9
Jon Dehdari <jon@dehdari.org>
2016-02-17 16:38:44 +0100

    tok-tok.pl: mv trailing space removal down; misc. cleanup

--- tok-tok.pl
+++ tok-tok.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 ## Simple, general tokenizer, where the input has one sentence per line (thus only final period is tokenized)
-## By Jon Dehdari, 2011-2015
+## By Jon Dehdari, 2011-2016
 ## Changes this:  They thought, "Is 9.5 or 525,600 my favorite number?"  before seeing Dr. Bob's dog talk.
 ## To this:       They thought , " Is 9.5 or 525,600 my favorite number ? " before seeing Dr. Bob ' s dog talk .
 
@@ -16,7 +16,7 @@ my $lower = undef;
 my $digit = 0;
 
 my $usage     = <<"END_OF_USAGE";
-tok-tok.pl    (c) 2011-2015 Jon Dehdari - Apache License v2
+tok-tok.pl    (c) 2011-2016 Jon Dehdari - Apache License v2
 
 Usage:    perl $0 [options] < text.txt > text.tok.txt
 
@@ -54,20 +54,20 @@ while (<>) {
   s/\|/ &#124; /g;		# replace problematic character with numeric character reference
   s/(\p{Open_Punctuation})/ $1 /g;
   s/(\p{Close_Punctuation})/ $1 /g;
-  s/(,{2,})/ $1 /g;			# Fake German,Czech, etc.: „
-  s/(?<!,)([,،])(?![,\d])/ $1 /g;		# don't tokenize 1,000,000
-  s/([({\[“‘„‚«‹「『])/ $1 /g;
+  s/(,{2,})/ $1 /g;		# fake German,Czech, etc.: „
+  s/(?<!,)([,،])(?![,\d])/ $1 /g;	# don't tokenize 1,000,000
+  s/([({\[“‘„‚«‹「『])/ $1 /g;	# misc. opening punctuation
   s/(['’`])/ $1 /g;		# just tokenize problematic hyphen/single quote, etc.
   s/ ` ` / `` /g;		# stupid quotes
   s/ ' ' / '' /g;		# stupid quotes
   s/(\p{Currency_Symbol})/ $1 /g;
-  s/([–—])/ $1 /g;		# En dash and em dash
-  s/\s+$/\n/g;			# rm trailing spaces
-  s/(-{2,})/ $1 /g;			# Fake en-dash, etc.
+  s/([–—])/ $1 /g;		# en dash and em dash
+  s/(-{2,})/ $1 /g;		# fake en-dash, etc.
   s/(\.{2,})/ $1 /g;	# treat multiple periods as a thing (eg. ellipsis)
-  s/(?<!\.)\.$/ ./g;			# don't tokenize period unless it ends the line (and isn't preceded by another period)
-  s/(?<!\.)\.\s*(["'’»›”]) *$/ . $1/g;			# don't tokenize period unless it's near the end of the line: eg. " ... stuff."
-  s/^ +//g;				# rm leading  spaces
+  s/(?<!\.)\.$/ ./g;	# don't tokenize period unless it ends the line (and isn't preceded by another period)
+  s/(?<!\.)\.\s*(["'’»›”]) *$/ . $1/g;	# don't tokenize period unless it's near the end of the line: eg. " ... stuff."
+  s/\s+$/\n/g;			# rm trailing spaces
+  s/^\s+//g;			# rm leading  spaces
   s/ {2,}/ /g;			# merge duplicate spaces
 
   $digit and s/\d/$digit/g;
